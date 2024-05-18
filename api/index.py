b1daca2ef42595ae745b8982be82092f2f71b00d
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2024-05-18 22:08:27 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,21 +3,38 @@ from flask import Flask, request
 from threading import Thread
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from hmac import compare_digest
 import time, requests, secrets, os
 
 password = os.getenv("mongodbpw")
+access_key = os.getenv("accesskey")
 uri = f"mongodb+srv://thecommcraft:{password}@cluster0.7xdht5m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about/')
+@app.get('/about/')
 def about():
     return str(tld_server.domains)
 
+@app.post('/new_domain/<domain_name>')
+def new_domain(domain_name):
+    data = request.json
+    key = data.get("access_key")
+    owner = data.get("owner")
+    if not compare_digest(key, access_key):
+        return jsonify({"success": False, "reason": "Unauthorized", "key": None})
+    if domain_name in dbdict:
+        return jsonify({"success": False, "reason": "Already exists", "key": None})
+    if not owner:
+        return jsonify({"success": False, "reason": "Needs an owner", "key": None})
+    key = secrets.token_urlsafe(32)
+    tld_server.add_domain(domain_name=domain_name, key=key, owner=owner)
+    return jsonify({"success": True, "reason": "Added", "key": key})
+    
 domains = {}
 db = client["db"]
 domains = db["domains"]
@@ -50,3 +67,7 @@ class DBDict:
 
 dbdict = DBDict(domains)
 tld_server = TLDServer(app=app, tlds=["site", "tcc"], domains=dbdict)
+
+@tld_server.on("set_domain_ip")
+def on_set_ip(event):
+    dbdict.coll.update_one({"domain": event.domain_name}, {"ip": event.ip})
